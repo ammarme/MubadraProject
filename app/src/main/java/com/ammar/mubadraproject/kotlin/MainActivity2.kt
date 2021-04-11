d57eb34fed64454df5fb32eb66ce4e97119e46a1
PC <ammaryasserr>
2021-04-11 21:32:52 +0200

    newact

---
+++ app/src/main/java/com/ammar/mubadraproject/kotlin/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.ammar.mubadraproject.kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ammar.mubadraproject.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
