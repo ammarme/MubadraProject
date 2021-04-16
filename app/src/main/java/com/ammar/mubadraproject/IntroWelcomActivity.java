37bf3bbef8adbb98841193f7c7a71b4faee50c44
Ammar <ammar.yasser.yossef.altabaa@gmail.com>
2021-04-17 01:02:39 +0200

    IntroWelcomActivity.java was added

--- app/src/main/java/com/ammar/mubadraproject/IntroWelcomActivity.java
+++ app/src/main/java/com/ammar/mubadraproject/IntroWelcomActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class IntroWelcomActivity extends AppCompatActivity {
-private  static  int SPLASH_SCREEN_TIME_OUT= 4000;
+private  static  int SPLASH_SCREEN_TIME_OUT= 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
