ca9727fa05db98314708672d5a8505bbe3e61808
Ammar <ammar.yasser.yossef.altabaa@gmail.com>
2021-04-17 00:29:10 +0200

    add new table input text

---
+++ app/src/main/java/com/ammar/mubadraproject/IntroWelcomActivity.java
@@ -0,0 +1,28 @@
+package com.ammar.mubadraproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class IntroWelcomActivity extends AppCompatActivity {
+private  static  int SPLASH_SCREEN_TIME_OUT= 4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_welcom_);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent  = new Intent(IntroWelcomActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+                }
+
+
+        },SPLASH_SCREEN_TIME_OUT);
+    }
+}
--- app/src/main/java/com/ammar/mubadraproject/fragments/AboutUsFragment.java
+++ app/src/main/java/com/ammar/mubadraproject/fragments/AboutUsFragment.java
@@ -1,3 +1,4 @@
+
 package com.ammar.mubadraproject.fragments;
 
 import android.app.Fragment;
